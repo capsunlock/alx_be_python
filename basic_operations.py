aada033f8513a9e3477d6c0a8944ab68cc7117a5
Caps Unlock <capsonluck@gmail.com>
2025-11-09 16:18:52 +0100

    simple_interest.py

--- basic_operations.py
+++
@@ -1,8 +0,0 @@
-number1 = 1
-number2 = 2
-
-print("Addiction of", number1, "and", number2, "is", number1 + number2)
-
-print("Subtraction of", number1, "and", number2, "is", number1 - number2)
-
-print("Multiplication of", number1, "and", number2, "is", number1 * number2)
