53f87a8e2bb446ed224d4f2685c30a43f8fdd542
Caps Unlock <capsonluck@gmail.com>
2025-11-09 16:09:16 +0100

    basic_operations

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 1
+number2 = 2
+
+print("Addiction of", number1, "and", number2, "is", number1 + number2)
+
+print("Subtraction of", number1, "and", number2, "is", number1 - number2)
+
+print("Multiplication of", number1, "and", number2, "is", number1 * number2)
