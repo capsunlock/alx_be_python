c6871a71ff05eef0fd11fdb04fce9c375a0db515
Caps Unlock <capsonluck@gmail.com>
2025-11-09 16:32:33 +0100

    rectangle_area.py

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,12 @@
+# length of the rectangle
+length = 10
+
+# width of the rectangle
+width  = 5
+
+# Calculation --- Area = length * width
+# The result is stored in 'area'.
+area = length * width
+
+# --- Output ---
+print("The area of the rectangle is:", area)
