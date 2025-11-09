7f82f47d3bc5c829404f606f6cc3517d025658d4
Caps Unlock <capsonluck@gmail.com>
2025-11-09 16:24:30 +0100

    simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,15 @@
+# P: Principal amount
 principal = 1000
+
+# R: Annual interest rate 
 rate = 0.05
+
+# T: Time 
 time = 3
 
-print("The simple interest is:", principal * rate * time)
+# Calculation --- Formula: Interest (I) = Principal (P) * Rate (R) * Time (T)
+# The result is stored in 'interest'.
+interest = principal * rate * time
+
+# --- Output ---
+print(f"The simple interest is: {interest}")
