988af420a7968296c3afa43348f1ec62f2090317
Caps Unlock <capsonluck@gmail.com>
2025-12-01 18:43:35 +0100

    restructure daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -3,6 +3,11 @@ priority = input ("Priority (high/medium/low): ").lower()
 time_bound = input ("Is it time-bound? (yes/no): ").lower()
 
 match priority:
+  case "high":
+    if time_bound == "yes":
+      print(f"'{task}' is a {priority} priority task that requires immediate attention today!")
+    else:
+      print(f"'{task}' is a {priority} priority task. Consider completing it when you have free time.")
   case _:
     if time_bound == "yes":
       print(f"'{task}' is a {priority} priority task that requires immediate attention today!")
